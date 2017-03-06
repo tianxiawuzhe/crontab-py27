0722c901c1a0d5b60cc8dfbcb52981a83a6171bc
tianxiawuzhe <j_l2006@163.com>
2017-03-06 18:02:17 +0800

    'first'

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
+set -x
 
 today=`date +"%Y%m%d"`
 
 appdir="/code"
 
 printenv | sed 's/^\(.*\)$/export \1/g' | grep 'BIT_' > ${appdir}/project_env.sh
-##cat ${appdir}/project_env.sh
-ls -l /var/spool/cron/crontabs/  ${appdir}/
-cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
-#service cron restart
+cat ${appdir}/project_env.sh
 
+echo "copy time zone to localtime ..."
+ls -l /var/spool/cron/crontabs/  ${appdir}/
+
+cp /usr/share/zoneinfo/Asia/Shanghai /etc/localtime
+
+echo "restart contab services ..."
+service cron restart
+
+echo "entrypoint end "
 #tail -f ${appdir}/jobs.log
